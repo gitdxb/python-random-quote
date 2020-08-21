e936002b62b777322caa3fc2e4174cf9f8907cbf
gitdxb <trantambls@gmail.com>
2020-08-21 21:21:25 +0400

    added new quotes and print more than 1 quote

--- get-quote.py
+++ get-quote.py
@@ -1,14 +1,19 @@
 import random
 def primary():
   #print("Keep it logically awesome.")
+ # f = open("quotes.txt", "a")
+ # f.write("Better late than never")
+ # f.close()
 
  f = open("quotes.txt")
  quotes = f.readlines()
  f.close()
 
+
  last = 13
  rnd = random.randint(0, last)
- print(quotes[rnd])
+ rnd1 = random.randint(0, last)
+ print(quotes[rnd]  + quotes[rnd1])
 
 if __name__== "__main__":
   primary()
